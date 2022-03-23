dae24f574cd75c45a56f5bd117b4a7c36908920a
LucasFasolato <89876311+LucasFasolato@users.noreply.github.com>
2022-03-23 12:42:58 -0300

    Modify styles, add new FontFamily
    Modify NavBar position to Absolute

--- rosarionights/src/FrontPage/FrontPage.js
+++ rosarionights/src/FrontPage/FrontPage.js
@@ -44,14 +44,14 @@ function FrontPage() {
                     <div className="front__text">
                         <p> 
                             Rosario Nights es un proyecto pensado para contener todas las opciones de la noche rosarina en un solo lugar.
-                            <br></br>
+                            <br/><br/>
                             Tendrás acceso a diferentes propuestas como por ejemplo: los boliches que abran durante esa fecha, bares y opciones para salir a tomar algo, lugares recomendados por la comunidad.
-                            <br></br>
-                            Además está en desarrollo la opción de poder chatear con personas que busquen realizar planes similares al tuyo y que puedan organizar algún encunetro previo o posterior a la salida (previar o hacer after).
+                            <br/><br/>
+                            Además está en desarrollo la opción de poder chatear con personas que busquen realizar planes similares al tuyo y que puedan organizar algún encuentro previo o posterior a la salida (previar o hacer after).
                         </p> 
                     </div> 
                 </div>
-                <div className='diplay__front-right'>
+                <div className='diplay__front-options'>
                     <div className='display__button'>
                         <button className='button__op boliche__bg'><p className='text__button'>BOLICHES</p></button>   
                     </div>  
@@ -64,6 +64,47 @@ function FrontPage() {
                     <div className='display__button'>
                         <button className='button__op comunidad__bg'><p className='text__button'>COMUNIDAD</p></button>   
                     </div>  
+                </div>
+                <div className='diplay__front-right'>
+                    <div className='display__right-title'>
+                        <p>Actividad de amigos</p>
+                        <button className='fas fa-user-friends'></button>
+                    </div>
+                    <p>Deja que otros amigos y seguidores vean tu actividad semanal.</p>
+                    <div className='display__friends'>
+                        <div className='display__friends-info'>
+                            <button className='fas fa-user button__friends-info'></button>
+                            <div className='text__friends-info'>
+                                <p>USERNAME</p>
+                                <p>MAIL</p>
+                                <p>LAST ACTIVITY</p>  
+                            </div> 
+                        </div>
+                        <div className='display__friends-info'>
+                            <button className='fas fa-user button__friends-info'></button>
+                            <div className='text__friends-info'>
+                                <p>USERNAME</p>
+                                <p>MAIL</p>
+                                <p>LAST ACTIVITY</p>  
+                            </div> 
+                        </div>
+                        <div className='display__friends-info'>
+                            <button className='fas fa-user button__friends-info'></button>
+                            <div className='text__friends-info'>
+                                <p>USERNAME</p>
+                                <p>MAIL</p>
+                                <p>LAST ACTIVITY</p>  
+                            </div> 
+                        </div>
+                        <div className='display__friends-info'>
+                            <button className='fas fa-user button__friends-info'></button>
+                            <div className='text__friends-info'>
+                                <p>USERNAME</p>
+                                <p>MAIL</p>
+                                <p>LAST ACTIVITY</p>  
+                            </div> 
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='display__footer'>
