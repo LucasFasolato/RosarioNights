9c96d8dcd081ef88cd14f2f9f70bc94210d64569
LucasFasolato <89876311+LucasFasolato@users.noreply.github.com>
2022-03-24 15:42:19 -0300

    Add imgs to Boliche's box, crate Boliche's fronPage

--- rosarionights/src/Boliches/Boliches.js
+++ rosarionights/src/Boliches/Boliches.js
@@ -8,7 +8,44 @@ function Boliches() {
     <div>
         <div className='boliches__bg'>
             <NavBar/>
-            <h1 className='title__boliches'>BOLICHES PAGE</h1> 
+            <div>
+                <h1 className='title__boliches'>BOLICHES PAGE</h1> 
+                <div className='display__op'>
+                    <div className='display__box'>
+                        <button className='display__btt fondo__blue'> 
+                        </button>
+                    </div>
+                    <div className='display__box'>
+                        <button className='display__btt fondo__volt'> 
+                        </button>
+                    </div>
+                    <div className='display__box'>
+                        <button className='display__btt fondo__bresh'> 
+                        </button>
+                    </div>
+                    <div className='display__box'>
+                        <button className='display__btt fondo__bohemia'> 
+                        </button>
+                    </div>
+                    <div className='display__box'>
+                        <button className='display__btt fondo__mora'> 
+                        </button>
+                    </div>
+                    <div className='display__box'>
+                        <button className='display__btt fondo__lamarina'> 
+                        </button>
+                    </div>
+                    <div className='display__box'>
+                        <button className='display__btt fondo__bound'> 
+                        </button>
+                    </div>
+                    <div className='display__box'>
+                        <button className='display__btt fondo__wallas'> 
+                        </button>
+                    </div>
+                </div>
+            </div>
+            
         </div>
         <Footer/>
     </div>   
